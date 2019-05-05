20b511a238208129cf54dfe790858a0c5790c9f4
BelleMardy <belle.belle@my.jcu.edu.au>
2019-05-05 10:03:25 +1000

    test re testing

--- app/src/main/java/au/edu/jcu/cp3406/test/MainActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/test/MainActivity.java
@@ -3,6 +3,7 @@ package au.edu.jcu.cp3406.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// test
 public class MainActivity extends AppCompatActivity {
 
     @Override
